504dee8ec72bbcb00026e726e845e45aaad34228
Pavel Vorobyov <aquavitale@yandex.ru>
2021-02-18 16:55:55 +0300

    interface changed slightly

--- indexable.go
+++ indexable.go
@@ -17,3 +17,41 @@ type Indexable interface {
 	Ref() interface{}
 	Type() IndexableType
 }
+
+// Object is an Indexable implementation helper type
+type Object struct {
+	id      string
+	ref     interface{}
+	bounds  *rtreego.Rect
+	objType IndexableType
+}
+
+// ID implements Indexable
+func (o *Object) ID() string {
+	return o.id
+}
+
+// Bounds implements Indexable
+func (o *Object) Bounds() *rtreego.Rect {
+	return o.bounds
+}
+
+// Ref implements Indexable
+func (o *Object) Ref() interface{} {
+	return o.ref
+}
+
+// Type implements Indexable
+func (o *Object) Type() IndexableType {
+	return o.objType
+}
+
+// NewObject creates a new instance of Object
+func NewObject(id string, objType IndexableType, bounds *rtreego.Rect, ref interface{}) *Object {
+	return &Object{
+		id:      id,
+		objType: objType,
+		bounds:  bounds,
+		ref:     ref,
+	}
+}
