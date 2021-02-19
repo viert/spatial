9438b29809fcffdf9472db75f48305f76b2a5700
Pavel Vorobyov <aquavitale@yandex.ru>
2021-02-19 12:25:43 +0300

    helper math fixed

--- helper/helper.go
+++ helper/helper.go
@@ -6,19 +6,18 @@ import (
 	"github.com/dhconnelly/rtreego"
 )
 
-func nmToLatLon(x float64, y float64, atLatitude float64) []float64 {
+func nmToLatLon(latSizeNM float64, lngSizeNM float64, atLatitude float64) []float64 {
 	// for latitude 60nm is 1˚
-	latSize := (1 / 60) * y
+	latSize := (1.0 / 60) * latSizeNM
 
 	// for longitude it depends on current latitude
-
 	// first let's convert latitude degree to radians
 	latitudeRad := (atLatitude * 2 * math.Pi) / 360
 
 	// calculate size
-	lngSize := (1 / 60) * x
+	lngSize := (1.0 / 60) * lngSizeNM
 	// and make a latitude correction
-	lngSize = lngSize / math.Cos(latitudeRad)
+	lngSize = lngSize / math.Abs(math.Cos(latitudeRad))
 	return []float64{latSize, lngSize}
 }
 
@@ -26,14 +25,16 @@ func nmToLatLon(x float64, y float64, atLatitude float64) []float64 {
 // Lat and Lng represent top left angle of the square
 func Square(lat float64, lng float64, sizeNM float64) *rtreego.Rect {
 	p := rtreego.Point{lat, lng}
-	rect, _ := rtreego.NewRect(p, nmToLatLon(sizeNM, sizeNM, lat))
+	sizes := nmToLatLon(sizeNM, sizeNM, lat)
+	rect, _ := rtreego.NewRect(p, sizes)
 	return rect
 }
 
 // SquareCentered makes square bounds of a given size
 // Lat and Lng represent center of the square
 func SquareCentered(lat float64, lng float64, sizeNM float64) *rtreego.Rect {
-	lat = lat - sizeNM/2
-	lng = lng - sizeNM/2
+	sizes := nmToLatLon(sizeNM/2, sizeNM/2, lat)
+	lat = lat - sizes[0]
+	lng = lng - sizes[1]
 	return Square(lat, lng, sizeNM)
 }
